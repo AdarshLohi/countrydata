2c75435b251539f9f1552697076842d3fff34000
adarshfreston <adarsh.lohithakshan@frestonanalytics.com>
2024-11-25 09:44:09 +0530

    suspence and fallback

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,29 +1,37 @@
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '@/pages/Home';
-import CountryDetails from '@/pages/CountryDetails';
 import ErrorBoundary from '@/components/ErrorBoundary';
+
+// Lazy loading components
+const Home = lazy(() => import('@/pages/Home'));
+const CountryDetails = lazy(() => import('@/pages/CountryDetails'));
+
+// Fallback component
+const Loading = () => <div>Loading...</div>;
 
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <ErrorBoundary>
-              <Home />
-            </ErrorBoundary>
-          }
-        />
-        <Route
-          path='details/:code'
-          element={
-            <ErrorBoundary>
-              <CountryDetails />
-            </ErrorBoundary>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <ErrorBoundary>
+                <Home />
+              </ErrorBoundary>
+            }
+          />
+          <Route
+            path='details/:code'
+            element={
+              <ErrorBoundary>
+                <CountryDetails />
+              </ErrorBoundary>
+            }
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
